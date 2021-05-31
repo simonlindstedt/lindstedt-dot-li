b3151978228e46fb066d6f0145ad8468fbc1e225
simonlindstedt <simon.lindstedt@gmail.com>
2021-05-31 17:01:03 +0200

    disabled sidescroll

--- src/components/Projects/styles.js
+++ src/components/Projects/styles.js
@@ -4,10 +4,10 @@ export const ProjectSection = styled.section`
   position: relative;
   min-height: 100vh;
   display: flex;
-  overflow-x: scroll;
-  @media (min-width: 1000px) {
-    flex-direction: column;
-    width: 100%;
-    overflow-x: hidden;
-  }
+  /* overflow-x: scroll; */
+  /* @media (min-width: 1000px) { */
+  flex-direction: column;
+  width: 100%;
+  /* overflow-x: hidden; */
+  /* } */
 `;
