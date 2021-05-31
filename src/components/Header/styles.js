031250b70d1aefc99969c8cbef30b023d60a91db
simonlindstedt <simon.lindstedt@gmail.com>
2021-05-31 12:02:59 +0200

    added fade-in animation

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
+  position: relative;
   min-height: calc(100vh - 24px);
   min-width: 100vw;
   background-color: black;
@@ -8,6 +9,7 @@ export const StyledHeader = styled.header`
   justify-content: center;
   align-items: center;
   h1 {
+    animation: fade-in 2s ease-in 0.5s 1 normal both;
     color: white;
     font-size: 8em;
   }
